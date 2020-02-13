221f7aaa042e978b110925ea12e17dfa036babe6
Lai_Khan <seogong9026@gmail.com>
2020-02-13 17:12:06 +0900

    3차
    - DB 처리 함수

--- server/lib/db.js
+++ server/lib/db.js
@@ -21,19 +21,28 @@ const addUser = async (name, roomCode) => {
 }
 
 const findUser = async (name, roomCode) => {
+    const existingUser = await User.findAll({ where: {name: name, roomCode: roomCode} });
 
+    if(existingUser) {
+        return { error: '이미 존재하는 이름입니다.'};
+    } else {
+        return false;
+    }
 }
 
 const getUsersInRoom = async (roomCode) => {
-
+    const result = await Room.findAndCount({ where: {roomCode: roomCode} });
+    return result;
 }
 
 const setRoom = async (roomCode, round, time) => {
-
+    const result = await Room.update( {round: round, time: time}, { where: {romCode: roomCode} });
+    return result;
 }
 
 const getRoomSetting = async (roomCode) => {
-
+    const result = await Room.findOne({ where: {code: roomCode} });
+    return result;
 }
 
 const setUserScore = async (name, roomCode, score) => {
@@ -42,7 +51,8 @@ const setUserScore = async (name, roomCode, score) => {
 }
 
 const getUserScore = async (name, roomCode) => {
-
+    const result = await User.findOne( {score: score}, { where: {name: name, roomCpde: roomCode} });
+    return result;
 }
 
 const deleteRoom = async (roomCode) => {
