68544e2234b2f8450cf562a2adda9c5ea6df609e
Shim MunSeong <puterism.k@gmail.com>
2020-02-13 16:12:51 +0900

    socket client 구현 중

--- client/src/modules/room.js
+++ client/src/modules/room.js
@@ -1,53 +1,98 @@
-export const FETCH_ROOM_ID = 'room/FETCH_ROOM_ID';
-export const FETCH_ROOM_ID_SUCCESS = 'room/FETCH_ROOM_ID_SUCCESS';
-export const FETCH_ROOM_ID_FAILURE = 'room/FETCH_ROOM_ID_FAILURE';
+// action types
+export const FETCH_ROOM_CODE = 'room/FETCH_ROOM_CODE';
+export const FETCH_ROOM_CODE_SUCCESS = 'room/FETCH_ROOM_CODE_SUCCESS';
+export const FETCH_ROOM_CODE_FAILURE = 'room/FETCH_ROOM_CODE_FAILURE';
 
-export const fetchRoomID = (nickname) => ({
-  type: FETCH_ROOM_ID,
+export const CONNECT_ROOM = 'room/CONNECT_ROOM';
+export const CONNECT_ROOM_SUCCESS = 'room/CONNECT_ROOM_SUCCESS';
+export const CONNECT_ROOM_FAILURE = 'room/CONNECT_ROOM_FAILURE';
+
+
+// action
+export const fetchRoomCode = (name) => ({
+  type: FETCH_ROOM_CODE,
   payload: {
-    nickname: nickname,
+    name: name,
   },
 });
 
-export const fetchRoomIDSuccess = (response) => ({
-  type: FETCH_ROOM_ID_SUCCESS,
+export const fetchRoomCodeSuccess = (response) => ({
+  type: FETCH_ROOM_CODE_SUCCESS,
   payload: response,
 });
 
-export const fetchRoomIDFailure = (response) => ({
-  type: FETCH_ROOM_ID_FAILURE,
+export const fetchRoomCodeFailure = (response) => ({
+  type: FETCH_ROOM_CODE_FAILURE,
   payload: response,
 });
 
+export const connectRoom = (name, code) => ({
+  type: CONNECT_ROOM,
+  payload: {
+    name: name,
+    code: code,
+  },
+});
+
+export const connectRoomSuccess = (response) => ({
+  type: CONNECT_ROOM_SUCCESS,
+  payload: response,
+});
+
+export const connectRoomFailure = (response) => ({
+  type: CONNECT_ROOM_FAILURE,
+  payload: response,
+});
 
 const initialState = {
-  id: '',
-  nickname: '',
+  code: '',
+  name: '',
+  connected: false,
 }
 
 // reducer
 function room(state = initialState, { type, payload }) {
   switch (type) {
-    case FETCH_ROOM_ID:
+    case FETCH_ROOM_CODE:
       return {
         ...state,
         status: 'FETCHING',
       }
-    case FETCH_ROOM_ID_SUCCESS: 
+    case FETCH_ROOM_CODE_SUCCESS: 
       return {
         ...state,
         ...payload.data,
-        nickname: payload.nickname,
+        name: payload.name,
       };
-    case FETCH_ROOM_ID_FAILURE:
+    case FETCH_ROOM_CODE_FAILURE:
       return {
         ...state,
         ...payload.response.data,
-        id: '',
-        nickname: '',
+        code: '',
+        name: '',
         showError: true,
       };
+
+    case CONNECT_ROOM:
+      return {
+        ...state,
+      };
     
+    case CONNECT_ROOM_SUCCESS:
+      return {
+        ...state,
+        ...payload.data,
+        connected: true,
+      };
+
+    case CONNECT_ROOM_FAILURE:
+      return {
+        ...state,
+        ...payload.response.data,
+        connected: false,
+        showError: true,
+      };
+
     default:
       return state;
   }
--- client/src/sagas/room.js
+++ client/src/sagas/room.js
@@ -1,25 +1,43 @@
 import { takeLatest, all, call, put } from 'redux-saga/effects';
+import { eventChannel } from 'redux-saga';
 import {
-  FETCH_ROOM_ID, FETCH_ROOM_ID_SUCCESS, FETCH_ROOM_ID_FAILURE,
+  FETCH_ROOM_CODE, FETCH_ROOM_CODE_SUCCESS, FETCH_ROOM_CODE_FAILURE,
 } from '../modules/room';
 import { push } from 'connected-react-router';
 import { createRoom } from '../apis';
+import io from 'socket.io-client';
 
+function connect() {
+  const socket = io('http://localhost:3001');
+  return new Promise(resolve => {
+    socket.on('connect', () => {
+      resolve(socket);
+    });
+  });
+}
+
+function subscribe(socket) {
+  return eventChannel(emit => {
+    socket.on('join', ({ name }) => {
+      emit('join')
+    })
+  })
+}
 function* fetchRoomIDSaga(action) {
   const { payload } = action;
   if (!payload) return;
 
   try {
     const response = yield call(createRoom, payload);
-    yield put({ type: FETCH_ROOM_ID_SUCCESS, payload: { ...response, ...payload }});
+    yield put({ type: FETCH_ROOM_CODE_SUCCESS, payload: { ...response, ...payload }});
     yield put(push(`/room/${response.data.id}`, { id: response.data.id }));
   } catch (error) {
-    yield put({ type: FETCH_ROOM_ID_FAILURE, payload: error });
+    yield put({ type: FETCH_ROOM_CODE_FAILURE, payload: error });
   }
 }
 
 export default function* roomSaga() {
   yield all([
-    takeLatest(FETCH_ROOM_ID, fetchRoomIDSaga),
+    takeLatest(FETCH_ROOM_CODE, fetchRoomIDSaga),
   ])
 }
