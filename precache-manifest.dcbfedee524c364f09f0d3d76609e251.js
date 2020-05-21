6512530413f749f4c9de061f890a59953737f111
Shim MunSeong <puterism.k@gmail.com>
2020-05-21 19:47:10 +0900

    Updates

---
+++ precache-manifest.dcbfedee524c364f09f0d3d76609e251.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63354b3b622bd84ee9212f1c5122b980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c2c4c7bc54fb4f19563",
+    "url": "/static/js/2.cf74ef1c.chunk.js"
+  },
+  {
+    "revision": "e2a595cebc19315c6e6690cf04f42e5b",
+    "url": "/static/js/2.cf74ef1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cddf47d3f0d7277121d1",
+    "url": "/static/js/main.09c7481b.chunk.js"
+  },
+  {
+    "revision": "913bfc28a065c1c6d964",
+    "url": "/static/js/runtime-main.4ccefbe5.js"
+  },
+  {
+    "revision": "bd84a0a65e4e35a6c731496f69755880",
+    "url": "/static/media/BG.bd84a0a6.svg"
+  },
+  {
+    "revision": "47a36e60491735eb01e8c905a29df3a0",
+    "url": "/static/media/Pictorial.47a36e60.svg"
+  },
+  {
+    "revision": "59f3fd4de09908f1ae2dcd2b6006b627",
+    "url": "/static/media/Planet.59f3fd4d.svg"
+  },
+  {
+    "revision": "de4fd36a585eaa86f4ad64cc9b791ed4",
+    "url": "/static/media/Stars.de4fd36a.svg"
+  },
+  {
+    "revision": "cbeab28f452a642ddd0d48162354623c",
+    "url": "/static/media/alienL1.cbeab28f.svg"
+  },
+  {
+    "revision": "7e8ad729b357010b31ff6200fe567aeb",
+    "url": "/static/media/alienL2.7e8ad729.svg"
+  },
+  {
+    "revision": "441cef012ea206184a8f9c9b7a3c471c",
+    "url": "/static/media/alienL3.441cef01.svg"
+  },
+  {
+    "revision": "b25226e5889713722bb50a5561dbf256",
+    "url": "/static/media/alienL4.b25226e5.svg"
+  },
+  {
+    "revision": "5e7f17859cbf6df5ab82e9599b2ee1bf",
+    "url": "/static/media/alienL5.5e7f1785.svg"
+  },
+  {
+    "revision": "5ae46582dbfdf6e51ea47be5742773ea",
+    "url": "/static/media/alienL6.5ae46582.svg"
+  },
+  {
+    "revision": "299bf00b824e887f3c2e706ff53ab9a5",
+    "url": "/static/media/alienL7.299bf00b.svg"
+  },
+  {
+    "revision": "cf5a7753134c859d2a672ad2c00f6665",
+    "url": "/static/media/alienL8.cf5a7753.svg"
+  },
+  {
+    "revision": "c8b6198b87027643960e802b492de45f",
+    "url": "/static/media/check.c8b6198b.svg"
+  },
+  {
+    "revision": "b244a567eb0f4468c4d5ecb6a8dac6cc",
+    "url": "/static/media/circle.b244a567.svg"
+  },
+  {
+    "revision": "03ff995f7ac4967a30d4066f58cae8b7",
+    "url": "/static/media/crown-1.03ff995f.svg"
+  },
+  {
+    "revision": "d75f89d942ffa95aa3222877f485deac",
+    "url": "/static/media/crown-2.d75f89d9.svg"
+  },
+  {
+    "revision": "f0534f04bea9e146018e3deb69b6c5b0",
+    "url": "/static/media/crown-3.f0534f04.svg"
+  },
+  {
+    "revision": "9a35e06e96bf66f123adf972a22a9f57",
+    "url": "/static/media/finding-mark.9a35e06e.svg"
+  },
+  {
+    "revision": "be7d3f7571e361642baed5198a3ef30a",
+    "url": "/static/media/flakes.be7d3f75.svg"
+  },
+  {
+    "revision": "baf4fc0ce171cd3bcf141f3bd1c6b4cc",
+    "url": "/static/media/found-mark.baf4fc0c.svg"
+  },
+  {
+    "revision": "c40f2c2d3f8aff594a26e10c271b6134",
+    "url": "/static/media/gamestart-with-help.c40f2c2d.svg"
+  },
+  {
+    "revision": "6b15269fae035e3d46ccea9305d91bc9",
+    "url": "/static/media/image-owner-mark.6b15269f.svg"
+  },
+  {
+    "revision": "a50973b75e04052d8e61a577951ab9e9",
+    "url": "/static/media/left.a50973b7.svg"
+  },
+  {
+    "revision": "8c1a1fdf1a6b7406d26ae19f9a404ff2",
+    "url": "/static/media/link-share-btn.8c1a1fdf.svg"
+  },
+  {
+    "revision": "4aadf8d7102fd235050b95a13187a1be",
+    "url": "/static/media/loading-wheel.4aadf8d7.svg"
+  },
+  {
+    "revision": "f606dd74948ab9b6e40d54d9304c1a40",
+    "url": "/static/media/loading.f606dd74.svg"
+  },
+  {
+    "revision": "160d5e1a84c1330535050467fbc37953",
+    "url": "/static/media/plus.160d5e1a.svg"
+  },
+  {
+    "revision": "e3cdf77283b21f21aa1c8199efa97d07",
+    "url": "/static/media/result.e3cdf772.svg"
+  },
+  {
+    "revision": "b46ca1d4e7f81bc5ff7e00bb2c3ffb42",
+    "url": "/static/media/right.b46ca1d4.svg"
+  },
+  {
+    "revision": "a5f4a7e33b42aa5823013eb5fcf584ee",
+    "url": "/static/media/scoreboard.a5f4a7e3.svg"
+  },
+  {
+    "revision": "2629c8186b9f3ae9611a5f70166dd1f5",
+    "url": "/static/media/small_circle.2629c818.svg"
+  }
+]);
